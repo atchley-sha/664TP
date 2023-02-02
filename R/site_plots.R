7d737cc7e2906a637e804bc2b68b9ac1e8e291c8
Hayden Atchley <atchley.sha@gmail.com>
2023-02-01 18:23:41 -0700

    update text plotting scripts

--- R/site_plots.R
+++ R/site_plots.R
@@ -1,64 +1,41 @@
 # Put traffic counts on site map
-plot_traffic_counts <- function(counts, image_file, development_name, out_file, scale = 0.965){
+plot_traffic_counts <- function(counts, image_file, development_name, out_file, dev_x = 975, dev_y = 950){
   
-p <- counts %>% 
-  ggplot() +
-  geom_blank() +
-  geom_text(aes(x = x, y = y, label = volume), size = 3) +
-  geom_text(aes(x = 50, y = 50, label = development_name), size = 3) +
-  coord_fixed() +
-  lims(x = c(0,100), y = c(0,80)) +
-  theme_void()
-
-ggdraw() +
-  draw_image(image_file, scale = scale) +
-  draw_plot(p)
-
-ggsave(out_file)
-
-p
-}
-
-
-# Put traffic LOS on site map
-plot_los <- function(LOS, LOS_int, image_file, development_name, out_file, scale = 0.965){
+  p <- image_file %>% 
+    image_read() %>% 
+    image_ggplot(interpolate = TRUE) +
+    geom_text(aes(x = dev_x, y = dev_y, label = development_name), size = 3) +
+    geom_text(data = counts, aes(x = x, y = y, label = volume), size = 3)
   
-  p <- LOS %>% 
-    ggplot() +
-    geom_blank() +
-    geom_text(aes(x = x, y = y, label = los), size = 3) +
-    geom_label(data = LOS_int, size = 5, mapping = aes(
-      x = x, y = y, labes = los, fill = "black", color = "white")) +
-    geom_text(aes(x = 50, y = 50, label = development_name), size = 3) +
-    coord_fixed() +
-    lims(x = c(0,100), y = c(0,80)) +
-    theme_void()
-  
-  ggdraw() +
-    draw_image(image_file, scale = scale) +
-    draw_plot(p)
-  
-  ggsave(out_file)
+  ggsave(out_file, plot = p)
   
   p
 }
 
+plot_los <- function(LOS, LOS_int, image_file, development_name, out_file, dev_x = 975, dev_y = 950){
+  
+  p <- image_file %>% 
+    image_read() %>% 
+    image_ggplot(interpolate = TRUE) +
+    geom_text(aes(x = dev_x, y = dev_y, label = development_name), size = 3) +
+    geom_text(data = LOS, aes(x = x, y = y, label = los), size = 3) +
+    geom_label(data = LOS_int, size = 5, mapping = aes(
+      x = x, y = y, labes = los, fill = "black", color = "white"))
+  
+  ggsave(out_file, plot = p)
+  
+  p
+}
 
 # Put development name on site map
-plot_map <- function(image_file, development_name, out_file, scale = 0.965){
+plot_map <- function(image_file, development_name, out_file, dev_x = 975, dev_y = 950){
   
-  p <- ggplot() +
-    geom_blank() +
-    geom_text(aes(x = 50, y = 50, label = development_name), size = 3) +
-    coord_fixed() +
-    lims(x = c(0,100), y = c(0,80)) +
-    theme_void()
+  p <- image_file %>% 
+    image_read() %>% 
+    image_ggplot(interpolate = TRUE) +
+    geom_text(aes(x = dev_x, y = dev_y, label = development_name), size = 3)
   
-  ggdraw() +
-    draw_image(image_file, scale = scale) +
-    draw_plot(p)
-  
-  ggsave(out_file)
+  ggsave(out_file, plot = p)
   
   p
 }
